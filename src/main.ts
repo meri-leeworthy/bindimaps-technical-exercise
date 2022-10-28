3cf675a7bbbe7fa8087b1a381dd387f6ba0e0014
Meri Leeworthy <meri.leeworthy@gmail.com>
2022-10-28 05:28:52 +0000

    draw venues and paths

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,38 @@
 import './style.css'
-import * from './models'
+import {Coordinate, SessionEvent, UserSession, Venue} from './models'
+import userSessions from './userSessions.json'
+import venues from './venues.json'
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    Hello
-  </div>
-`
+const canvas = document.querySelector<HTMLDivElement>('#app')!.appendChild(document.createElement('canvas'))
+canvas.width = 800
+canvas.height = 600
+
+const ctx = canvas.getContext('2d');
+
+const maxX = Math.max(...venues.map(venue => venue.position.x))
+const maxY = Math.max(...venues.map(venue => venue.position.y))
+
+function scale (x, y) {
+  return [(x*canvas.width)/(maxX + 50), (y*canvas.height)/(maxY + 50)]
+}
+
+function drawCircle (x, y) {
+  ctx.fillStyle = "red"
+  ctx.beginPath()
+  ctx.arc(x, y, 5, 0, Math.PI * 2, true)
+  ctx.fill()
+}
+
+function drawPath (path) {
+  ctx.strokeStyle = "green"
+  ctx.beginPath()
+  ctx.moveTo(...path[0])
+  path.forEach(point => ctx.lineTo(...point))
+  ctx.stroke()
+}
+
+const paths = userSessions.map(user => user.path.map(point => scale(point.position.x, point.position.y)))
+
+paths.forEach(path => drawPath(path))
+venues.forEach(venue => drawCircle(...scale(venue.position.x, venue.position.y)))
 
